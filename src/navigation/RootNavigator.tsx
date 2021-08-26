4871ff2f2a4b336b69e4472570665bdbd5bb738a
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-08-26 17:19:02 -0500

    added differernt options on bottom nav

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import React from "react"
-import HomeScreen from "../screens/HomeScreen"
 import LoadingScreen from "../screens/LoadingScreen"
 import ProblemSolverScreen from "../screens/ProblemSolverScreen"
+import BottomTabNavigator from "./BottomTabNavigator"
 
 export default function RootNavigator() {
   const Stack = createStackNavigator()
@@ -13,7 +13,7 @@ export default function RootNavigator() {
       <Stack.Navigator headerMode="none" mode="modal">
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="ProblemSolver" component={ProblemSolverScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Main" component={BottomTabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   )
