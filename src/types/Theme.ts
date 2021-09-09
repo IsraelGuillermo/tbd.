dc20f8964e599e05a1ec730f9efa999fa6bac0bd
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-09-09 16:53:54 -0500

    adding additional colors to useTheme

--- src/types/Theme.ts
+++ src/types/Theme.ts
@@ -3,6 +3,12 @@ export interface Colors {
   primary2: string
   primary3: string
   primary4: string
+  secondary1: string
+  secondary2: string
+  secondary3: string
+  secondary4: string
+  secondary5: string
+  secondary6: string
 }
 
 export interface Theme {
