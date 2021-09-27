d759a05ddd3babb4ac04cdcc386d7b202b6fc0fe
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-09-27 16:59:22 -0500

    committing changes to have coin flip and randomly land on tails or heads.

--- src/screens/CoinScreen/index.tsx
+++ src/screens/CoinScreen/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
-import React from "react"
-import { Button, StyleSheet, View } from "react-native"
+import React, { useEffect, useState } from "react"
+import { Animated, Button, StyleSheet, View } from "react-native"
 import Row from "../../components/Row"
 import SvgIcon from "../../components/SvgIcon"
 import Typography from "../../components/Typography"
@@ -38,8 +38,64 @@ export default function CoinScreen() {
     coinBox: {
       height: "85%",
       width: "100%",
+      alignContent: "center",
+      justifyContent: "center",
+    },
+    coin: {
+      width: 100,
+      height: 100,
+      position: "relative",
+      //   backfaceVisibility: "hidden",
+    },
+    heads: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "red",
+      position: "absolute",
+      //   transform: [{ rotateY: "180deg" }],
+      //   flex: 1,
+      backfaceVisibility: "hidden",
+    },
+    tails: {
+      position: "absolute",
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "blue",
+      backfaceVisibility: "hidden",
+      zIndex: 100,
+      //   flex: 1,
     },
   })
+  const [rotateAnimation] = useState(new Animated.Value(0))
+
+  const handleAnimation = () => {
+    Animated.timing(rotateAnimation, {
+      toValue: 1,
+      duration: 800,
+      useNativeDriver: true,
+    }).start(() => {
+      rotateAnimation.setValue(0)
+    })
+  }
+  const [turns, setTurns] = useState(0)
+
+  const interpolateRotating = rotateAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", `1440deg`],
+  })
+  const animatedStyle = {
+    transform: [
+      {
+        rotateY: interpolateRotating,
+      },
+    ],
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -57,7 +113,28 @@ export default function CoinScreen() {
         </Row>
       </View>
       <View style={styles.coinBox}>
-        <Button title={"Flip"} onPress={() => console.log("Clicked")} />
+        <Animated.View style={[animatedStyle, styles.coin]}>
+          {turns > 0.5 ? (
+            <View style={styles.heads}>
+              <Typography variant={"heading1"}>Heads</Typography>
+            </View>
+          ) : (
+            <View style={styles.tails}>
+              <Typography variant={"heading1"}>Tails</Typography>
+            </View>
+          )}
+        </Animated.View>
+
+        <Button
+          title={"Flip"}
+          onPress={() => {
+            handleAnimation()
+
+            setTimeout(() => {
+              setTurns(Math.random())
+            }, 500)
+          }}
+        />
       </View>
     </View>
   )
