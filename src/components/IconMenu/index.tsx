7536b9fa4f756b0063b8ae7fdef366ee76374ac2
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-10-07 18:09:42 -0500

    commiting style changes to icon menu

--- src/components/IconMenu/index.tsx
+++ src/components/IconMenu/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { FlatList, StyleSheet, View } from "react-native"
+import { FlatList, StyleSheet, TouchableHighlight, View } from "react-native"
 import { useTheme } from "../../theme"
 import { SvgIcons } from "../../types/SvgIcons"
 import Icon from "../Icon"
@@ -39,8 +39,8 @@ export function IconMenu() {
   ]
   const styles = StyleSheet.create({
     iconMenu: {
-      width: 100,
-      height: 150,
+      width: 200,
+      height: 125,
       backgroundColor: theme.colors.secondary7,
       borderRadius: 8,
       overflow: "hidden",
@@ -49,14 +49,26 @@ export function IconMenu() {
   return (
     <View style={styles.iconMenu}>
       <FlatList
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: 8,
+        }}
+        numColumns={3}
         data={iconData}
         renderItem={({ item }) => (
-          <Icon
-            key={item.backgroundColor}
-            iconName={item.iconName}
-            backgroundColor={item.backgroundColor}
-            style={{ margin: 8 }}
-          />
+          <TouchableHighlight
+            style={{ borderRadius: 8 }}
+            onPress={() => console.log(item.iconName)}
+          >
+            <Icon
+              key={item.backgroundColor}
+              iconName={item.iconName}
+              backgroundColor={item.backgroundColor}
+              style={{ margin: 8 }}
+            />
+          </TouchableHighlight>
         )}
       />
     </View>
